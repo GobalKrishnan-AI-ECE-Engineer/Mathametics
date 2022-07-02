54ed3d25e158af01967961af8b1543d37bdac4cd
gobalkrishnan-engineer <gobalkrishnan.developer@gmail.com>
2022-07-02 11:42:48 +0530

    path

---
+++ lib/util/common_title_in.dart
@@ -0,0 +1,145 @@
+var mm010101 = ["Counting with small numbers", "Counting in order"];
+var mm010102 = ["Comparing numbers of objects", "Counting by category"];
+var mm010103 = ["Intro to addition"];
+var mm010104 = ["Making 5"];
+var mm010105 = ["Getting to 10 by filling boxes", "Adding to 10"];
+var mm010106 = ["Intro to subtraction"];
+var mm010107 = ["Add and subtract: pieces of fruit", "Addition and subtraction within 10"];
+var mm010108 = ["Relating addition and subtraction"];
+var mm010109 = ["Addition word problems within 10", "Subtraction word problems within 10"];
+
+var mm0101 = [
+  "Counting small numbers",
+  mm010101,
+  "Comparing small numbers",
+  mm010102,
+  "What is additions?",
+  mm010103,
+  "Making small numbers",
+  mm010104,
+  "Making 10",
+  mm010105,
+  "What is subtraction?",
+  mm010106,
+  "Subtract within 10",
+  mm010107,
+  "Relate addition and subtraction",
+  mm010108,
+  "Addition and subtraction word problems",
+  mm010109,
+];
+
+var mm010201 = [
+  "Teens as sums with 10",
+  "Teen numbers: monkeys",
+  "Intro to place value",
+];
+var mm010202 = ["Counting in pictures", "Counting objects 1", "Counting objects 2"];
+var mm010203 = ["Adding 7 + 6", "Adding 8 + 7", "Adding 5 + 3 + 6"];
+var mm010204 = ["Subtracting 14 - 6"];
+var mm010205 = ["Equal sign"];
+var mm010206 = [""];
+var mm010207 = ["Repeated addition: haircuts"];
+var mm010208 = [
+  "Addition and subtraction word problems: superheroes",
+  "Addition and subtraction word problems: gorillas"
+];
+var mm010209 = ["Comparison word problems: marbles", "Comparison word problems: roly-polies"];
+
+var mm0102 = [
+  "Counting numbers from 1 to 20",
+  mm010201,
+  "Counting objects",
+  mm010202,
+  "Addition within 20",
+  mm010203,
+  "Subtraction within 20",
+  mm010204,
+  "Equal sign",
+  mm010205,
+  "Missing numbers within 20",
+  mm010206,
+  "Repeated addition",
+  mm010207,
+  "Word problems within 20",
+  mm010208,
+  "Word problems with 'more' and 'fewer'",
+  mm010209,
+];
+var mm010301 = ["Number grid"];
+var mm010302 = ["Counting by tens"];
+var mm010303 = ["Intro to place value", "Place value example: 25", "Place value example: 42"];
+var mm010304 = ["Greater than and less than symbols"];
+
+var mm0103 = [
+  "Counting numbers to 100",
+  mm010301,
+  "Counting by tens",
+  mm010302,
+  "Place value",
+  mm010303,
+  "Comparing 2-digit numbers",
+  mm010304,
+];
+
+var mm010401 = [
+  "Relative position",
+];
+var mm010402 = ["Ordering by length"];
+
+var mm0104 = [
+  "Shapes and space",
+  mm010401,
+  "Length and size",
+  mm010402,
+];
+
+var mm01 = [
+  "Numbers from 1 to 9",
+  mm0101,
+  "Numbers from 10 to 20",
+  mm0102,
+  "Numbers from 1 to 100",
+  mm0103,
+  "Geometry and measurement",
+  mm0104,
+];
+var mm02 = [];
+var mm03 = [];
+var mm04 = [];
+var mm05 = [];
+var mm06 = [];
+var mm07 = [];
+var mm08 = [];
+var mm09 = [];
+var mm0A = [];
+var mm0B = [];
+var mm0C = [];
+
+var mm0 = [
+  "Class 1",
+  mm01,
+  "Class 2",
+  mm02,
+  "Class 3",
+  mm03,
+  "Class 4",
+  mm04,
+  "Class 5",
+  mm05,
+  "Class 6",
+  mm06,
+  "Class 7",
+  mm07,
+  "Class 8",
+  mm08,
+  "Class 9",
+  mm09,
+  "Class 10",
+  mm0A,
+  "Class 11",
+  mm0B,
+  "Class 12",
+  mm0C,
+];
+var m0 = ["Math - India", mm0];
