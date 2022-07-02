21736ab5c29d76d9fa5d9c13154c9852dcfd9cfa
gobalkrishnan-engineer <gobalkrishnan.developer@gmail.com>
2022-07-02 12:34:37 +0530

    class 2

--- lib/util/common_title_in.dart
+++ lib/util/common_title_in.dart
@@ -104,7 +104,104 @@ var mm01 = [
   "Geometry and measurement",
   mm0104,
 ];
-var mm02 = [];
+
+var mm020101 = ["Number grid"];
+var mm020102 = ["Intro to place value", "Place value example: 25", "Place value example: 42"];
+var mm020103 = ["Greater than and less than symbols"];
+
+var mm0201 = [
+  "Counting numbers to 100",
+  mm020101,
+  "Place value",
+  mm020102,
+  "Comparing 2-digit numbers",
+  mm020103,
+];
+
+var mm020201 = [
+  "Adding 1 vs. adding 10",
+  "Understanding place value when adding ones",
+  "Understanding place value when adding tens"
+];
+var mm020202 = [
+  "Subtracting 1 vs. subtracting 10",
+  "Subtracting 1s using place value",
+  "Subtracting 10 using place value",
+];
+var mm020203 = [
+  "Adding 2-digit numbers without regrouping 1",
+  "Breaking apart 2-digit addition problems"
+];
+var mm020204 = ["Subtracting two-digit numbers without regrouping"];
+var mm020205 = [
+  "Addition and subtraction with number lines",
+  "Adding and subtracting on number line word problems"
+];
+
+var mm0202 = [
+  "Adding 1s and 10s",
+  mm020201,
+  "Subtracting 1s and 10s",
+  mm020202,
+  "Adding without regrouping",
+  mm020203,
+  "Subtracting without regrouping",
+  mm020204,
+  "Addition and subtraction using number line",
+  mm020205,
+];
+
+var mm020301 = [
+  "Regrouping to add 1-digit number",
+  "Adding by making a group of 10",
+  "Adding 53+17 by making a group of 10"
+];
+var mm020302 = ["Subtracting with regrouping (borrowing)"];
+var mm020303 = ["Missing numbers in addition and subtraction"];
+var mm020304 = [
+  "Addition word problems: horses",
+  "Subtraction word problem: tennis balls",
+  "Subtraction word problem: snow",
+  "Subtraction word problem: crayons",
+];
+var mm020305 = ["Addition word problem: starfish", "Subtraction word problem: basketball"];
+
+var mm0203 = [
+  "Adding with regrouping",
+  mm020301,
+  "Subtracting with regrouping",
+  mm020302,
+  "Addition and subtraction mixed problems",
+  mm020303,
+  "Word problems within 100",
+  mm020304,
+  "'Fewer' and 'More' word problems",
+  mm020305,
+];
+
+var mm020401 = ["Comparing lengths", "Estimating lengths"];
+var mm020402 = ["Understanding mass (grams and kilograms)"];
+var mm020403 = ["Understanding volume (liters)"];
+
+var mm0204 = [
+  "Length",
+  mm020401,
+  "Mass",
+  mm020402,
+  "Volume",
+  mm020403,
+];
+
+var mm02 = [
+  "Numbers from 1 to 100",
+  mm0201,
+  "Addition and subtraction without regrouping",
+  mm0202,
+  "Addition and subtraction with regrouping",
+  mm0203,
+  "Geometry and measurement",
+  mm0204,
+];
 var mm03 = [];
 var mm04 = [];
 var mm05 = [];
